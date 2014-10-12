d26eba0f9763d5071b0f95058267f2656d429fa3
charliedowler <charliedowler15@gmail.com>
2014-10-12 22:41:39 +0100

    Searching jobs (WIP)

--- assets/view/HeaderMenu.js
+++ assets/view/HeaderMenu.js
@@ -1,7 +1,36 @@
-var React = require('react');
+var _ = require('underscore');
+var React = require('react/addons');
+var Jobs = require('../collection/Jobs');
 
 module.exports = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
+  getInitialState: function() {
+    return {
+      query: null,
+      jobs: [],
+      problem: null,
+      results: []
+    };
+  },
+  componentWillMount: function() {
+    var self = this;
+    var jobs = new Jobs();
+    jobs.root = this.props.root;
+    jobs.on('all', function () {
+      self.setState({ jobs: jobs });
+    });
+    jobs.fetch({
+      error: function (models, response) {
+        self.setState({ error: {
+          problem: response.statusCode
+        }})
+      }
+    });
+  },
   render: function () {
+    var results = this.state.results.map(function(result) {
+      return <li>{result.get('name')}</li>;
+    });
     return <div className="ui inverted menu">
       <a className="active item">
         <i className="home icon"></i>
@@ -12,15 +41,31 @@ module.exports = React.createClass({
       <div className="right menu">
         <div className="item">
           <div className="ui icon input">
-            <input type="text" placeholder="Search..." />
+            <input type="text" onChange={this.handleQuery} defaultValue={this.state.query} placeholder="Search..." />
             <i className="search link icon"></i>
           </div>
+          <ul className={!this.state.results.length ? 'dropdown hidden' : 'dropdown'}>{results}</ul>
         </div>
         <a className="item" href="#" onClick={this.logout}>
           <i className="sign out icon"></i>
         </a>
       </div>
     </div>;
+  },
+  handleQuery: function(event) {
+    var query = event.target.value;
+
+    if (!query) {
+      this.setState({ query: null, results: [] });
+      return false;
+    }
+
+    var results = this.state.jobs.filter(function(job) {
+      var matcher = new RegExp(query, 'i');
+      return matcher.test(job.get('name'));
+    });
+
+    this.setState({ query: query, results: results});
   },
   logout: function() {
     if (Storage) {
