5f62b3a622ad5a6650387608e97f82eca0bb4007
charliedowler <charliedowler15@gmail.com>
2014-10-23 23:16:57 +0100

    JobView (WIP)

--- assets/view/JobView.js
+++ assets/view/JobView.js
@@ -6,19 +6,53 @@ module.exports = React.createClass({
   mixins: [APIMixin, StateMixin],
   getInitialState: function() {
     return {
-      report: null,
-      info: null
+      job: null,
+      report: {},
+      info: {}
     };
   },
   componentWillMount: function() {
-    var job = this.props.job;
+    this.setBuildReport(this.props.job);
+  },
+  render: function() {
+    return <div> {this.generateTitle()} </div>;
+  },
+  setBuildReport: function(job) {
     this.execute('getLastBuildReport', job, function(err, res) {
       if (err) throw new Error(err);
       var report = JSON.parse(res);
-      this.setState({ report: report});
+      this.setState({ job: job, report: report });
     }.bind(this));
   },
-  render: function() {
-    return <div> {this.props.job} </div>;
+  generateTitle: function() {
+    var status;
+
+    switch(this.state.report.result) {
+      case 'SUCCESS':
+        status = <b>passing.</b>;
+        break;
+      case 'FAILURE':
+        status = <b>failing.</b>;
+        break;
+      case 'UNSTABLE':
+        status = <b>unstable.</b>;
+        break;
+      case 'NOT_BUILT':
+        status = <b>not built.</b>;
+        break;
+      case 'ABORTED':
+        status = <b>aborted</b>;
+        break;
+      default:
+        status = <b>...</b>;
+    }
+    return <h1>
+      {this.state.job} is {status}
+    </h1>;
+  },
+  componentWillReceiveProps: function(nextProps) {
+    if (nextProps.job != this.state.job) {
+      this.setBuildReport(nextProps.job);
+    }
   }
 });
